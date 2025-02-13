943eea8f26dda91ea82eba18a13ac714debe0dae
Angus MacMinn <angus13@hotmail.ca>
2025-02-12 16:16:08 -0800

    some css edits

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,7 @@ import { NavLink } from 'react-router-dom';
 const Nav = () => (
     <nav>
     <ul>
-      {/* <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
-      <li><NavLink to="/movie" activeClassName="active">Single Page</NavLink></li> */}
-      <li><NavLink to="/about-us" className={({ isActive }) => isActive ? "active" : ""}>About Us</NavLink></li>
+      <li><NavLink to="/about-us" className={({ isActive }) => isActive ? "active" : ""}>About</NavLink></li>
       <li><NavLink to="/favourites" className={({ isActive }) => isActive ? "active" : ""}>Favourites</NavLink></li>
     </ul>
   </nav>
